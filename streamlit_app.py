f9dbae6d5bd20c4e2c51b8a9eb4dfdef4b264371
kylesammons <141177424+kylesammons@users.noreply.github.com>
2024-11-26 13:20:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,48 +12,24 @@ from google.oauth2 import service_account
 st.set_page_config(page_title="The Reef", page_icon="🎫")
 st.title("🎫 The Reef")
 
+# Create the BigQuery client
+credentials = service_account.Credentials.from_service_account_file('/trimark-tdp-0b42c5927efa.json')
+client = bigquery.Client(credentials=credentials, project='trimark-tdp')
+
+# Fetch data from BigQuery
+query = """
+  SELECT
+    *
+  FROM
+    `trimark-tdp.reference.paidmedia_ref`
+
+
+"""
+df = client.query(query).to_dataframe()
+
 # Create a random Pandas dataframe with existing tickets.
 if "df" not in st.session_state:
 
-    # Set seed for reproducibility.
-    np.random.seed(42)
-
-    # Make up some fake issue descriptions.
-    issue_descriptions = [
-        "Network connectivity issues in the office",
-        "Software application crashing on startup",
-        "Printer not responding to print commands",
-        "Email server downtime",
-        "Data backup failure",
-        "Login authentication problems",
-        "Website performance degradation",
-        "Security vulnerability identified",
-        "Hardware malfunction in the server room",
-        "Employee unable to access shared files",
-        "Database connection failure",
-        "Mobile application not syncing data",
-        "VoIP phone system issues",
-        "VPN connection problems for remote employees",
-        "System updates causing compatibility issues",
-        "File server running out of storage space",
-        "Intrusion detection system alerts",
-        "Inventory management system errors",
-        "Customer data not loading in CRM",
-        "Collaboration tool not sending notifications",
-    ]
-
-    # Generate the dataframe with 100 rows/tickets.
-    data = {
-        "ID": [f"TICKET-{i}" for i in range(1100, 1000, -1)],
-        "Issue": np.random.choice(issue_descriptions, size=100),
-        "Status": np.random.choice(["Open", "In Progress", "Closed"], size=100),
-        "Priority": np.random.choice(["High", "Medium", "Low"], size=100),
-        "Date Submitted": [
-            datetime.date(2023, 6, 1) + datetime.timedelta(days=random.randint(0, 182))
-            for _ in range(100)
-        ],
-    }
-    df = pd.DataFrame(data)
 
     # Save the dataframe in session state (a dictionary-like object that persists across
     # page runs). This ensures our data is persisted when the app updates.
