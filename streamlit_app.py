991baaf3635fc1522a55aa0761bcab6dfb28cedb
kylesammons <141177424+kylesammons@users.noreply.github.com>
2024-11-26 15:01:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,3 +71,24 @@ if submitted:
     # Update session state and save to CSV
     st.session_state.df = pd.concat([st.session_state.df, df_new], axis=0, ignore_index=True)
     st.session_state.df.to_csv(csv_file, index=False)
+
+# Show and edit existing tickets
+st.header("The Reef")
+st.write(f"Number of tickets: `{len(st.session_state.df)}`")
+st.info(
+    "You can edit the tickets by double-clicking on a cell. Note how the plots below "
+    "update automatically! You can also sort the table by clicking on the column headers.",
+    icon="✍️"
+)
+
+# Editable table
+edited_df = st.data_editor(
+    st.session_state.df,
+    use_container_width=True,
+    hide_index=True,
+)
+
+# Save changes back to CSV
+if not edited_df.equals(st.session_state.df):
+    st.session_state.df = edited_df
+    st.session_state.df.to_csv(csv_file, index=False)
